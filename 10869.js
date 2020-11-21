81b004b275263b23175e734e67e530f9af8e83bb
pikokr <parangee9706@gmail.com>
2020-11-21 20:44:22 +0900

    10869

---
+++ 10869.js
@@ -0,0 +1,16 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.on('line', line => {
+    console.log(line.split(' ').map(r=>Number(r)).reduce((acc,cur) => acc+cur))
+    console.log(line.split(' ').map(r=>Number(r)).reduce((acc,cur) => acc-cur))
+    console.log(line.split(' ').map(r=>Number(r)).reduce((acc,cur) => acc*cur))
+    console.log(line.split(' ').map(r=>Number(r)).reduce((acc,cur) => Math.floor(acc/cur)))
+    console.log(line.split(' ').map(r=>Number(r)).reduce((acc,cur) => acc%cur))
+
+    rl.close()
+}).on('close', () => process.exit())
