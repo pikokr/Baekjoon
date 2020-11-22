92ac86dafed45d5b0e3abd6d90459ee8b076321d
pikokr <parangee9706@gmail.com>
2020-11-22 11:03:02 +0900

    2439

---
+++ 2439.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int cnt;
+    cin >> cnt;
+    for (int i = 1; i <= cnt; ++i) {
+        for (int j = 0; j < cnt - i; ++j) {
+            cout << ' ';
+        }
+        for (int j = 0; j < i; ++j) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
