5b58b4ba288fa4cc0deb4aa9a75a7fa10f5cfdc4
pikokr <parangee9706@gmail.com>
2020-11-23 11:39:35 +0900

    2442

---
+++ src/boj2442/main.kt
@@ -0,0 +1,10 @@
+package boj2442
+
+fun main() {
+    val c = readLine()!!.toInt()
+    for (i in 1..c) {
+        print(" ".repeat(c-i))
+        print("*".repeat(i*2-1))
+        println()
+    }
+}
