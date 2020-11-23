aefccd7ffac76367e18ab675ac9b9dffda397791
pikokr <parangee9706@gmail.com>
2020-11-23 11:20:14 +0900

    1977

---
+++ src/boj1977/main.kt
@@ -0,0 +1,15 @@
+package boj1977
+
+fun main() {
+    val m = readLine()!!.toInt()
+    val n = readLine()!!.toInt()
+    val numbers = ArrayList<Int>()
+    var i = 0
+    while (i * i <= n) {
+        if (i * i in m..n) {
+            numbers.add(i * i)
+        }
+        i++
+    }
+    println(if (numbers.size == 0) -1 else "${numbers.reduce { acc, cur -> acc + cur }}\n${numbers[0]}")
+}
