cfb4265ce90941f0a4b4d40dce324bd4a2708c1b
pikokr <parangee9706@gmail.com>
2020-11-23 11:35:18 +0900

    2441

--- src/boj2441/main.kt
+++ src/boj2441/main.kt
@@ -1,8 +1,9 @@
-package boj2440
+package boj2441
 
 fun main() {
     val c = readLine()!!.toInt()
     for (i in c downTo 1) {
+        print(" ".repeat(c-i))
         println("*".repeat(i))
     }
 }
